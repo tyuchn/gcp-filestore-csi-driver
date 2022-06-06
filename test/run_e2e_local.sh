8a73d16fd614d65841a821023777a5c5c9abed17
tyuchn <tyuchn@google.com>
2022-06-05 21:18:18 -0700

    deprecated

--- test/run_e2e_local.sh
+++ test/run_e2e_local.sh
@@ -5,4 +5,4 @@ set -o errexit
 
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-filestore-csi-driver
 
-ginkgo -v -trace "${PKGDIR}/test/e2e/tests" --logtostderr -- --project ${PROJECT} --service-account ${GCFS_IAM_NAME}
+ginkgo -v -trace "${PKGDIR}/test/e2e/tests" --logtostderr -- --project "tyuchn-test" --service-account "gcp-filestore-csi-driver-sa@tyuchn-test.iam.gserviceaccount.com"
