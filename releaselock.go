6d05f55471a762f8bf8bcee1fc5ffba47bfcee2e
tyuchn <tyuchn@google.com>
2023-02-01 09:41:27 -0800

    convert ip to bytes

--- releaselock.go
+++ releaselock.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"io"
 	"log"
+	"math/big"
 	"net"
 	"net/rpc"
 	"strconv"
 
 	"github.com/prashanthpai/sunrpc"
 )
+
+func IP4toInt(IPv4Address net.IP) int64 {
+	IPv4Int := big.NewInt(0)
+	IPv4Int.SetBytes(IPv4Address.To4())
+	return IPv4Int.Int64()
+}
+
+//similar to Python's socket.inet_aton() function
+//https://docs.python.org/3/library/socket.html#socket.inet_aton
+
+func Pack32BinaryIP4(ip4Address string) []byte {
+	ipv4Decimal := IP4toInt(net.ParseIP(ip4Address))
+
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, uint32(ipv4Decimal))
+
+	if err != nil {
+		fmt.Println("Unable to write to buffer:", err)
+	}
+
+	// // present in hexadecimal format
+	// result := fmt.Sprintf("%x", buf.Bytes())
+	return buf.Bytes()
+}
 
 func main() {
 
@@ -52,7 +80,7 @@ func main() {
 
 	log.Printf("Calling IN_BAND_PROPRIETARY_LOCK_OPS_PROG.IN_BAND_PROPRIETARY_LOCK_OPS_V1 to release lock for node ip %q", clientNodeIp)
 
-	err = client.Call("IN_BAND_PROPRIETARY_LOCK_OPS_PROG.IN_BAND_PROPRIETARY_LOCK_OPS_V1", clientNodeIp, nil)
+	err = client.Call("IN_BAND_PROPRIETARY_LOCK_OPS_PROG.IN_BAND_PROPRIETARY_LOCK_OPS_V1", Pack32BinaryIP4(clientNodeIp), nil)
 	if err != nil {
 		log.Print("client.Call() failed: ", err)
 	}
